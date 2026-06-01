bbe96b8a72d89804cf3757d0eb8d740ad8b9a04f
Kerem <90683756+Kerem-Tutucu@users.noreply.github.com>
2026-06-01 13:34:54 +0200

    #3 Formatting semantic HTML
    #3 Formatting semantic HTML

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,30 @@
+<script>
+	import heartIcon from '$lib/assets/heart.svg';
+	import turkiyeFlag from '$lib/assets/turkiye-icon.svg';
+    import heart from '$lib/assets/heart.svg';
+</script>
+
+<header>
+	<nav>
+		<a href="/">
+			Hüdayi Gençlik
+		</a>
+
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/over-ons">Over Ons</a></li>
+			<li><a href="/projecten">Projecten</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+
+		<a href="/doneren">
+			<span>Online bağış</span>
+			<img src={heartIcon} alt="" />
+		</a>
+
+		<a href="/tr">
+			<img src={turkiyeFlag} alt="" />
+			<span>Türkçe</span>
+		</a>
+	</nav>
+</header>
