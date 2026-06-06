e38d6618cf8b7916a63066916322661def5fc2b3
Recep <recep680@outlook.com>
2026-06-06 12:04:34 +0200

    [#18] feat: added and styled instagram widget

---
+++ src/lib/components/ui/InstagramWidget.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+    <!-- Elfsight Instagram Feed | Untitled Instagram Feed -->
+    <script src="https://elfsightcdn.com/platform.js" async></script>
+</svelte:head>
+
+<section class="section instagram-widget">
+    <h2>Bizi Instagram'da takip edin</h2>
+    <div class="widget-container">
+        <div class="elfsight-app-7e2d56d4-05e4-4bee-b528-aa040cea4e02" data-elfsight-app-lazy></div>
+    </div>
+</section>
+
+<style>
+    .instagram-widget {
+        background: var(--c-creme);
+        text-align: center;
+        h2 {
+            font-size: 40px;
+            color: var(--c-nachtgroen);
+        }
+        .widget-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+</style>
